3d2c4ca3fc8345fa6ef21a2448d819807ecb9935
Evaketesdi <evahollo85@yahoo.com>
2023-09-22 20:58:05 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ae483c2e-ebca-4b61-b761-5b4e7ae70c2b';
+export const MODAL_CLOSE_SEC = 2.5;
